a90c773ee9ffd7ca5e47d533264c15ce1bd3ce8a
EladShaba <Shaba.elad@gmail.com>
2025-02-09 15:43:10 +0200

    updated route

--- backend/routes/recipe_routes.py
+++ backend/routes/recipe_routes.py
@@ -11,17 +11,18 @@ from DB.fridge.get_fridges import get_fridges_from_db
 # - fill recpie = .... with the right function
 recipe_bp = Blueprint('recipe_bp', __name__)
 
-@recipe_bp.route('/get_recipe', methods=['POST'])
+@recipe_bp.route('/get_recipe', methods=['GET'])
 def get_recipe():
     try:
         # data is user_id
+    
         user_id = request.args.get("user_id")
         if not user_id:
             return jsonify({"error": "user_id query parameter is required."}), 400
         
         # Get fridges
         fridges = get_fridges_from_db(user_id)
-        fridges_list = [{"fridge_id": row[0], "fridge_name": row[1]} for row in fridges]
+        #fridges_list = [{"fridge_id": row[0], "fridge_name": row[1]} for row in fridges]
 
         # Get products for each fridge: need to get the procut, amount, expdate
         ingridients = None # fill
